e0d145c99ffb07ba07caf1087445ca5405b062a2
nfamily0 <nfamily123@gmail.com>
2020-03-03 20:26:10 +0900

    삼각형 생성 성공

---
+++ LearnOpenGL/Triangle/main.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+//정점 쉐이더 코드 작성
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+
+//뷰포트 변경시 작동하는 콜백함수
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// 입력값을 받아서 처리
+void processInput(GLFWwindow* window);
+
+int main() {
+	//glfw 초기화
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	//glfw window 생성
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//뷰포트 설정
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//GLAD 초기화
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//정점쉐이더 생성
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//정점쉐이더에 소스코드 연결
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//정점쉐이더 컴파일
+	glCompileShader(vertexShader);
+
+	//컴파일 성공 여부
+	int  success;
+	char infoLog[512]; //에러 메세지가 생길경우 여기에 생성됨
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//플래그먼트 쉐이더 생성
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//소스코드를 플레그먼트 에 연결
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//컴파일
+	glCompileShader(fragmentShader);
+	//컴파일 성공 여부 채크
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//쉐이더 를 연결할 프로그램 ID 생성
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	//쉐이더를 붙혀줌
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//shader linking
+	glLinkProgram(shaderProgram);
+
+	//linking 성공 여부 채크
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	//사용한 쉐이더 삭제
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//삼각형 정점 설정
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	//Vertex Buffer object(VBO),Vertex Array Object(VAO)
+	unsigned int VBO, VAO;
+	//정점버퍼의 GPU메모리상의 ID생성
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//정점 Array Buffer 바인드
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//바인드된 VBO에 float배열 복사
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//정점속성 설정
+	//1. 매개변수 : shader 소스코드의 (location = 0)에 대응하는 정점속성 생성
+	//2. 정점 크기 지정 vec3이므로 3
+	//3. 데이터 타입
+	//4. 정규화 여부
+	//5. stride 설정 정정버퍼의 비트단위 크기 지정
+	//6. 정점 시작 위치 (void*)로 형변환 해주어야 함
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//glVertexAttribPointer를 호출하면 VBO가 정점 속성의 바운드 정점 버퍼 객체로 등록되어 나중에 안전하게 바인딩 해제
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
+	//render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		//gl배경색
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		//삼각형 그리기
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); //
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+
+	//사용한 버퍼 버퍼들 삭제
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	//glfw 자원 정리
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
